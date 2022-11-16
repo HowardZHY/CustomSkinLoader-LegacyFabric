46250e85fd27610547c2f738a1a9092469133100
Howard ZHY <96782166+HowardZHY@users.noreply.github.com>
2022-11-17 00:55:15 +0800

    WIPing

--- src/main/java/net/minecraft/resources/IResource.java
+++
@@ -1,9 +0,0 @@
-package net.minecraft.resources;
-
-import java.io.InputStream;
-
-// this class shouldn't be created by ASM because of modlauncher issue.
-// exclude
-public interface IResource {
-    InputStream getInputStream();
-}
