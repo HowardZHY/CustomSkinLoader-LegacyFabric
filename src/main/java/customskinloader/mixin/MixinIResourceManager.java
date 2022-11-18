c46dab375737f8bae9731f4ecbbffa2cdc27bc06
Howard ZHY <96782166+HowardZHY@users.noreply.github.com>
2022-11-18 17:33:07 +0800

    Delete src/main directory

--- src/main/java/customskinloader/mixin/MixinIResourceManager.java
+++
@@ -1,9 +0,0 @@
-package customskinloader.mixin;
-
-import customskinloader.fake.itf.IFakeIResourceManager;
-import net.minecraft.resource.ResourceManager;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(ResourceManager.class)
-public interface MixinIResourceManager extends IFakeIResourceManager {
-}
