1515a068ec7f5e1d5811c8d04145414a43ccaa16
Howard ZHY <96782166+HowardZHY@users.noreply.github.com>
2022-11-19 19:24:22 +0800

    fix classcast crash

--- src/main/java/customskinloader/utils/MinecraftUtil.java
+++ src/main/java/customskinloader/utils/MinecraftUtil.java
@@ -27,19 +27,16 @@ public class MinecraftUtil {
 	public static File getMinecraftDataDir(){
 		if(minecraftDataFolder!=null)
 			return minecraftDataFolder;
-		testProbe();
-		if(minecraftDataFolder!=null)
-			return minecraftDataFolder;
 		return new File("");
 	}
 	
 	private static ArrayList<String> minecraftVersion=new ArrayList<String>();
-	private static String minecraftMainVersion=null;
+
+	private static String minecraftMainVersion = "1.7.10";
+
 	private final static Pattern MINECRAFT_VERSION_PATTERN = Pattern.compile(".*?(\\d+\\.\\d+[\\.]?\\d*).*?");
+
 	public static ArrayList<String> getMinecraftVersions(){
-		if(minecraftVersion!=null&&!minecraftVersion.isEmpty())
-			return minecraftVersion;
-		testProbe();
 		return minecraftVersion;
 	}
 	public static String getMinecraftVersionText(){
@@ -49,20 +46,6 @@ public class MinecraftUtil {
 		return StringUtils.trim(sb.toString());
 	}
 	public static String getMinecraftMainVersion(){
-		if(minecraftMainVersion!=null)
-			return minecraftMainVersion;
-		for(String version:getMinecraftVersions()){
-			Matcher m = null;
-			try{
-				m=MINECRAFT_VERSION_PATTERN.matcher(version);
-			}catch(Exception e){
-				e.printStackTrace();
-			}
-			if(m==null || !m.matches())
-				continue;
-			minecraftMainVersion=m.group(m.groupCount());
-			break;
-		}
 		return minecraftMainVersion;
 	}
 	
@@ -74,55 +57,28 @@ public class MinecraftUtil {
 		return data.address;
 	}
 	// ip:port
+
 	public static String getStandardServerAddress(){
 		return HttpUtil0.parseAddress(getServerAddress());
 	}
+
 	public static boolean isLanServer(){
 		return HttpUtil0.isLanServer(getStandardServerAddress());
 	}
+
 	public static String getCurrentUsername(){
 		return net.minecraft.client.MinecraftClient.getInstance().getSession().getProfile().getName();
 	}
 	
 	private final static Pattern MINECRAFT_CORE_FILE_PATTERN = Pattern.compile("^(.*?)/versions/([^\\/\\\\]*?)/([^\\/\\\\]*?).jar$");
-	private static void testProbe(){
-		minecraftVersion.clear();
-		URLClassLoader ucl = (URLClassLoader)new MinecraftUtil().getClass().getClassLoader();
-		URL urls[] = ucl.getURLs();
-		for(URL url:urls){
-			Matcher m = null;
-			try{
-				m=MINECRAFT_CORE_FILE_PATTERN.matcher(URLDecoder.decode(url.getPath(),"UTF-8"));
-			}catch(Exception e){
-				e.printStackTrace();
-			}
-			if(m==null || !m.matches())
-				continue;
-			if(minecraftDataFolder==null)
-				minecraftDataFolder=new File(m.group(1));
-			minecraftVersion.add(m.group(2));
-		}
-	}
+
 	public static boolean isCoreFile(URL url){
-		Matcher m = null;
-		try{
-			m=MINECRAFT_CORE_FILE_PATTERN.matcher(URLDecoder.decode(url.getPath(),"UTF-8"));
-		}catch(Exception e){
-			e.printStackTrace();
-			return false;
-		}
-		return m!=null && m.matches();
+        return false;
 	}
 	
 	private final static Pattern LIBRARY_FILE_PATTERN = Pattern.compile("^(.*?)/libraries/(.*?)/([^\\/\\\\]*?).jar$");
+
 	public static boolean isLibraryFile(URL url){
-		Matcher m = null;
-		try{
-			m=LIBRARY_FILE_PATTERN.matcher(URLDecoder.decode(url.getPath(),"UTF-8"));
-		}catch(Exception e){
-			e.printStackTrace();
-			return false;
-		}
-		return m!=null && m.matches();
+		return false;
 	}
 }
