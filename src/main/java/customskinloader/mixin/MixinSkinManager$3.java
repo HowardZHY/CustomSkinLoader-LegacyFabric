e42cdcebee5685c40615720d4435138c50c702c8
Howard ZHY <howardzhy128@163.com>
2023-02-20 04:37:54 +0800

    I think should working now

--- src/main/java/customskinloader/mixin/MixinSkinManager$3.java
+++ src/main/java/customskinloader/mixin/MixinSkinManager$3.java
@@ -1,26 +1,19 @@
 package customskinloader.mixin;
 
-import java.io.File;
-import java.util.Map;
-
 import com.mojang.authlib.GameProfile;
 import com.mojang.authlib.minecraft.MinecraftProfileTexture;
 import com.mojang.authlib.minecraft.MinecraftSessionService;
 import customskinloader.fake.FakeSkinManager;
-import net.minecraft.client.renderer.texture.TextureManager;
-import net.minecraft.client.resources.SkinManager;
-import net.minecraft.client.resources.SkinManager$3;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
+import java.util.Map;
+
 // For 1.12.2-
 @Mixin(targets = "net.minecraft.client.resources.SkinManager$3")
-public abstract class MixinSkinManager$3 extends SkinManager implements Runnable{
-    public MixinSkinManager$3(TextureManager textureManagerInstance, File skinCacheDirectory, MinecraftSessionService sessionService) {
-        super(textureManagerInstance, skinCacheDirectory, sessionService);
-    }
+public abstract class MixinSkinManager$3 implements Runnable{
+    // public MixinSkinManager$3(TextureManager textureManagerInstance, File skinCacheDirectory, MinecraftSessionService sessionService) {}
 
     @Redirect(
             method = "run()V",
